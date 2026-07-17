43c2f0ee192cc19948b4f1789011bf7f3311ece5
tiagoz5 <tiago.z5@gmail.com>
2026-07-17 18:37:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker — cache para funcionamento offline
-const CACHE = "postos-full-v30";
+const CACHE = "postos-full-v32";
 const ARQUIVOS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-full.png"];
 
 self.addEventListener("install", e => {
